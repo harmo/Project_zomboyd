4cadc0bd4e3657e1bc6de2029facce98b3af57a5
harmo <dtharmo@gmail.com>
2013-11-29 21:52:37 +0100

    Refactored some dirty code

--- zomboyd/camera.py
+++ zomboyd/camera.py
@@ -14,6 +14,9 @@ class Camera(object):
         self.inset_y = 96
         self.x_offset = 0
         self.y_offset = 0
+        self.outset = 20
+        self.min_x = self.max_x = 0
+        self.min_y = self.max_y = 0
 
     def add_world(self, world):
         self.world = world
@@ -23,38 +26,11 @@ class Camera(object):
         self.target = target
         self.rect.center = self.target.rect.center
 
-    def render_map(self, screen, map, layer, plane=None):
-        def blit_tile(x, y, tile_x, tile_y):
-            image = map.positions_list[layer][x][y][2]
-            screen.blit(image, (tile_x, tile_y))
-
-        for x in map.positions_list[layer]:
-            for y in map.positions_list[layer][x]:
-                tile_x = map.positions_list[layer][x][y][0]-self.rect.left
-                tile_y = map.positions_list[layer][x][y][1]-self.rect.top/2-self.inset_y
-
-                target_pos_y = HALF_HEIGHT-self.world.map.tile_height*2-self.target.rect.height
-
-                if layer is BACKGROUND_LAYER:
-                    blit_tile(x, y, tile_x, tile_y)
-                elif plane is 'first' and (tile_y >= target_pos_y or x >= HALF_WIDTH):
-                    blit_tile(x, y, tile_x, tile_y)
-                elif plane is 'second' and (tile_y < target_pos_y or x < HALF_WIDTH):
-                    blit_tile(x, y, tile_x, tile_y)
-
-        if DEBUG:
-            for collide in map.unwalkable:
-                pygame.draw.rect(self.world.screen, (255, 255, 255), collide, 1)
-
-    def render_player(self, screen, player):
-        player_tile, player_x, player_y = player.render()
-        screen.blit(player_tile, (player_x, player_y))
-
     def update(self):
         if self.target.is_moving:
+            xx, yy = self.world.get_cell(HALF_WIDTH, HALF_HEIGHT-self.world.map.tile_height*2)
 
             if self.target.is_colliding:
-                xx, yy = self.world.get_cell(HALF_WIDTH, HALF_HEIGHT-self.world.map.tile_height*2)
                 directions = self.world.map.get_properties(xx, yy, 'collide')
                 if not directions:
                     # Wall cell is not on current cell, check next
@@ -68,6 +44,9 @@ class Camera(object):
             # Add moves to offsets, to keep coords real after moves
             self.x_offset -= move_x
             self.y_offset -= move_y
+
+            self.min_x, self.min_y = xx-self.outset, yy-self.outset
+            self.max_x, self.max_y = xx+self.outset, yy+self.outset
 
             # Update all collidable rects
             # TODO loop only on visible ones
@@ -88,3 +67,32 @@ class Camera(object):
             self.rect.left += self.target.speed
 
         return (origin_x-self.rect.left, origin_y-self.rect.top/2)
+
+    def render_map(self, screen, map, layer, plane=None):
+        def blit_tile(x, y, tile_x, tile_y):
+            image = map.positions_list[layer][x][y][2]
+            screen.blit(image, (tile_x, tile_y))
+
+        for x in map.positions_list[layer]:
+            if x >= self.min_x and x <= self.max_x:
+                for y in map.positions_list[layer][x]:
+                    if y >= self.min_y and y <= self.max_y:
+                        tile_x = map.positions_list[layer][x][y][0]-self.rect.left
+                        tile_y = map.positions_list[layer][x][y][1]-self.rect.top/2-self.inset_y
+
+                        target_pos_y = HALF_HEIGHT-self.world.map.tile_height*2-self.target.rect.height
+
+                        if layer is BACKGROUND_LAYER:
+                            blit_tile(x, y, tile_x, tile_y)
+                        elif plane is 'first' and (tile_y >= target_pos_y or x >= HALF_WIDTH):
+                            blit_tile(x, y, tile_x, tile_y)
+                        elif plane is 'second' and (tile_y < target_pos_y or x < HALF_WIDTH):
+                            blit_tile(x, y, tile_x, tile_y)
+
+        if DEBUG:
+            for collide in map.unwalkable:
+                pygame.draw.rect(self.world.screen, (255, 255, 255), collide, 1)
+
+    def render_player(self, screen, player):
+        player_tile, player_x, player_y = player.render()
+        screen.blit(player_tile, (player_x, player_y))
