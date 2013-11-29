4e3e666d86fea7e749b5735c5a649f70a3f84f2d
harmo <dtharmo@gmail.com>
2013-11-29 20:28:37 +0100

    Added medias

---
+++ medias/Maps/TileWalls.tsx
@@ -0,0 +1,381 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TileWalls" tilewidth="64" tileheight="128">
+ <image source="../../old/graphics/zomboid/TileWalls.png" width="512" height="1024"/>
+ <tile id="0">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="" value=""/>
+   <property name="collideN" value=""/>
+   <property name="collideW" value=""/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="" value=""/>
+   <property name="collideN" value=""/>
+   <property name="collideW" value=""/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collide" value="l,t_l,t"/>
+   <property name="cutW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collide" value="t,t_r,r"/>
+   <property name="cutN" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="" value=""/>
+   <property name="collide" value="l,t_l,t,t_r,r"/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="" value=""/>
+   <property name="collideN" value=""/>
+   <property name="collideW" value=""/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="cutW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="cutN" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="WallW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="WallN" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="WallNW" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="WallSE" value=""/>
+   <property name="unflamable" value=""/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="" value=""/>
+   <property name="collideN" value=""/>
+   <property name="collideW" value=""/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="47">
+  <properties>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="collide" value="l,t_l,t"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="collide" value="t,t_r,r"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="collide" value="l,t_l,t,t_r,r"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="WallSE" value=""/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="" value=""/>
+   <property name="collideN" value=""/>
+   <property name="collideW" value=""/>
+   <property name="cutN" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="" value=""/>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="WindowW" value=""/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="WindowN" value=""/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="DoorWallW" value=""/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="DoorWallN" value=""/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="collideW" value=""/>
+   <property name="cutW" value=""/>
+   <property name="transparentW" value=""/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="collideN" value=""/>
+   <property name="cutN" value=""/>
+   <property name="transparentN" value=""/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="cutW" value=""/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="cutN" value=""/>
+  </properties>
+ </tile>
+</tileset>
